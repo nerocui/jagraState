0e20723ac3c2b9aa7ff0c820e1c342cc552090bd
nerocui <nerocui@outlook.com>
2019-11-20 15:37:32 -0800

    added dependencies, rooturl

--- action/index.ts
+++ action/index.ts
@@ -1,9 +1,8 @@
 import Axios from 'axios';
 import TYPE from './type';
-import { loadTheme } from 'office-ui-fabric-react/lib/Styling';
-import { GetRootURL } from '../utils/DomainService';
 import { TaskForCreationDto } from '../models';
 
+const RootUrl = 'https://jagra.azurewebsites.net';
 
 function DispatchLogin(token: string) {
 	return {
@@ -49,7 +48,7 @@ function AddTask(task: any) {
 
 export function CreateTask(task: TaskForCreationDto) {
 	return (dispatch: any) => {
-		Axios.post(GetRootURL() + '/api/task/create', {...task})
+		Axios.post(RootUrl + '/api/task/create', {...task})
 			.then(res => {
 				dispatch(AddTask(res.data));
 			})
@@ -61,7 +60,7 @@ export function CreateTask(task: TaskForCreationDto) {
 
 export function FetchOrganizationById(id: number) {
 	return (dispatch: any) => {
-		Axios.get(GetRootURL() + '/api/organization/' + id.toString())
+		Axios.get(RootUrl + '/api/organization/' + id.toString())
 			.then(res => {
 				console.log(res);
 				dispatch(SetOrganization(res.data));
@@ -74,7 +73,7 @@ export function FetchOrganizationById(id: number) {
 
 export function FetchOrganizationsByUser(userId: number) {
 	return (dispatch: any) => {
-		Axios.get(GetRootURL() + '/api/organization/byuser?userId=' + userId.toString())
+		Axios.get(RootUrl + '/api/organization/byuser?userId=' + userId.toString())
 			.then(res => {
 				dispatch(SetOrganizations(res.data));
 			})
@@ -86,7 +85,7 @@ export function FetchOrganizationsByUser(userId: number) {
 
 export function Login(username: string, password: string) {
 	return (dispatch: any) => {
-		Axios.post(GetRootURL() + '/api/auth/login', {username, password})
+		Axios.post(RootUrl + '/api/auth/login', {username, password})
 			.then(res => {
 				const { token } = res.data;
 				localStorage.setItem('token', token);
@@ -105,66 +104,16 @@ export function Logout() {
 	};
 }
 
-export function ToggleTheme() {
+export function ToggleTheme(loadTheme: any, value: any) {
 	return (dispatch: any) => {
 		const theme = localStorage.getItem('theme');
 		if (theme === 'light') {
 			localStorage.setItem('theme', 'dark');
-			loadTheme({
-				palette: {
-				  themePrimary: '#47afff',
-				  themeLighterAlt: '#03070a',
-				  themeLighter: '#0b1c29',
-				  themeLight: '#15354d',
-				  themeTertiary: '#2b6999',
-				  themeSecondary: '#3f9ae0',
-				  themeDarkAlt: '#5ab7ff',
-				  themeDark: '#73c3ff',
-				  themeDarker: '#98d2ff',
-				  neutralLighterAlt: '#262626',
-				  neutralLighter: '#2f2f2f',
-				  neutralLight: '#3d3d3d',
-				  neutralQuaternaryAlt: '#464646',
-				  neutralQuaternary: '#4d4d4d',
-				  neutralTertiaryAlt: '#6b6b6b',
-				  neutralTertiary: '#c8c8c8',
-				  neutralSecondary: '#d0d0d0',
-				  neutralPrimaryAlt: '#dadada',
-				  neutralPrimary: '#ffffff',
-				  neutralDark: '#f4f4f4',
-				  black: '#f8f8f8',
-				  white: '#1d1d1d',
-				}
-			});
+			loadTheme(value);
 			dispatch(SetTheme('dark'));
 		} else {
 			localStorage.setItem('theme', 'light');
-			loadTheme({
-				palette: {
-				  themePrimary: '#0078d4',
-				  themeLighterAlt: '#eff6fc',
-				  themeLighter: '#deecf9',
-				  themeLight: '#c7e0f4',
-				  themeTertiary: '#71afe5',
-				  themeSecondary: '#2b88d8',
-				  themeDarkAlt: '#106ebe',
-				  themeDark: '#005a9e',
-				  themeDarker: '#004578',
-				  neutralLighterAlt: '#f8f8f8',
-				  neutralLighter: '#f4f4f4',
-				  neutralLight: '#eaeaea',
-				  neutralQuaternaryAlt: '#dadada',
-				  neutralQuaternary: '#d0d0d0',
-				  neutralTertiaryAlt: '#c8c8c8',
-				  neutralTertiary: '#c2c2c2',
-				  neutralSecondary: '#858585',
-				  neutralPrimaryAlt: '#4b4b4b',
-				  neutralPrimary: '#333333',
-				  neutralDark: '#272727',
-				  black: '#1d1d1d',
-				  white: '#ffffff'
-				}
-			  });
+			loadTheme(value);
 			dispatch(SetTheme('light'));
 		}
 	}
